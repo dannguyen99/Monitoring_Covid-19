7870bc65eb520328a38afccb5948edb7e1bb21e1
huannguyen2008 <huannguyen2299@gmail.com>
2020-05-25 14:56:56 +0700

    commit to checkout

--- get_data/soup.py
+++ get_data/soup.py
@@ -1,5 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
-req = requests.get('https://vi.wikipedia.org/wiki/Rap')
+req = requests.get('https://vi.wikipedia.org/wiki/%C4%90%E1%BA%A1i_d%E1%BB%8Bch_COVID-19_t%E1%BA%A1i_Vi%E1%BB%87t_Nam')
 soup = BeautifulSoup(req.text, "lxml")
-print(soup.get_text())
+# data = soup.findAll("li", { "role":"button" })
+# names = [d.text for d in data]
+# print(names)
+print(soup.get_text)
