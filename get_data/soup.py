3d5111b50a86ee5ec82eb77d0ae562c3103a045b
huannguyen2008 <huannguyen2299@gmail.com>
2020-05-29 14:59:51 +0700

    co nhu khong co

--- get_data/soup.py
+++ get_data/soup.py
@@ -1,8 +1,18 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
-req = requests.get('https://vi.wikipedia.org/wiki/%C4%90%E1%BA%A1i_d%E1%BB%8Bch_COVID-19_t%E1%BA%A1i_Vi%E1%BB%87t_Nam')
+req = requests.get('https://ncov.moh.gov.vn/')
 soup = BeautifulSoup(req.text, "lxml")
-# data = soup.findAll("li", { "role":"button" })
-# names = [d.text for d in data]
-# print(names)
-print(soup.get_text)
+# table = soup.find("table",{"class":"wikitable sortable mw-collapsible"})
+print(soup.get_text())
+# output_rows = []
+# for table_row in table.findAll('tr'):
+#     columns = table_row.findAll('td')
+#     output_row = []
+#     for column in columns:
+#         output_row.append(column.text)
+#     output_rows.append(output_row)
+#
+# with open('output.csv', 'w') as csvfile:
+#     writer = csv.writer(csvfile)
+#     writer.writerows(output_rows)
