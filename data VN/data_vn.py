663c2f0bbf46e60c905c0fbdcbe3f484c3beef37
dannguyen <dannguyen0801@gmail.com>
2020-06-08 23:07:12 +0700

    update on logging and get data from vng

--- data VN/data_vn.py
+++ data VN/data_vn.py
@@ -2,11 +2,12 @@ import requests
 import csv
 from bs4 import BeautifulSoup
 
-req = requests.get('https://ncov.moh.gov.vn/', verify = False)
+req = requests.get('https://ncov.moh.gov.vn/', verify=False)
 soup = BeautifulSoup(req.text, "lxml")
 table_ncov = soup.find_all("table", {"class": "table table-striped table-covid19"})
 headers_city = "City, Total cases, Active, Recovered, Death"
 header_patient = "Patient number, Age, Gender, Location, Status, Nationality"
+
 
 def get_data_vn(table):
     output_rows = []
@@ -17,11 +18,14 @@ def get_data_vn(table):
             output_row.append(column.text)
         output_rows.append(output_row)
     return output_rows
+
+
 def makefile(file, output_rows, headers):
     with open(file, 'w') as csvfile:
         csvfile.write(headers)
         writer = csv.writer(csvfile)
         writer.writerows(output_rows)
 
-city = makefile('output_ct.csv',get_data_vn(table_ncov[0]),headers_city)
-patient = makefile('output_pt.csv',get_data_vn(table_ncov[1]),header_patient)
+
+makefile('output_ct.csv', get_data_vn(table_ncov[0]), headers_city)
+makefile('output_pt.csv', get_data_vn(table_ncov[1]), header_patient)
---
+++ execute.py
@@ -0,0 +1,66 @@
+import pandas as pd
+from datetime import datetime, timedelta
+import logging
+import schedule
+import time
+import requests
+import csv
+from bs4 import BeautifulSoup
+
+logging.basicConfig(filename='app.log', filemode='w', format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+
+# get data from John Hopkins University ( USA )
+def get_data_jhu():
+    yesterday = datetime.strftime(datetime.now() - timedelta(1), '%m-%d-%Y')
+    try:
+        data = pd.read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data"
+                           "/csse_covid_19_daily_reports/%s.csv" % yesterday)
+        data.to_csv('data/JHU/%s.csv' % yesterday)
+        logging.warning("Successfully updated data from JHU on %s" % yesterday)
+    except Exception as e:
+        logging.error(e)
+
+
+# get data from European Centre for Disease Prevention and Control ( EU )
+def get_data_ecdc():
+    yesterday = datetime.strftime(datetime.now() - timedelta(1), '%m-%d-%Y')
+    data = pd.read_csv("https://opendata.ecdc.europa.eu/covid19/casedistribution/csv")
+    data.to_csv('data/ECDC/%s.csv' % yesterday)
+    logging.warning("Successfully updated data from ECDC on %s" % yesterday)
+
+
+def get_data_vn():
+    req = requests.get('https://ncov.moh.gov.vn/', verify=False)
+    soup = BeautifulSoup(req.text, "lxml")
+    table_ncov = soup.find_all("table", {"class": "table table-striped table-covid19"})
+    headers = ["City, Total cases, Active, Recovered, Death",
+               "Patient number, Age, Gender, Location, Status, Nationality"]
+    yesterday = datetime.strftime(datetime.now() - timedelta(1), '%m-%d-%Y')
+    names = ["cities", "patients"]
+    for table, header, name in zip(table_ncov, headers, names):
+        output_rows = []
+        for table_row in table.findAll('tr'):
+            columns = table_row.findAll('td')
+            output_row = []
+            for column in columns:
+                output_row.append(column.text)
+            output_rows.append(output_row)
+        with open('data/VN/' + yesterday + '-' + name + '.csv', 'w') as csvfile:
+            csvfile.write(header)
+            writer = csv.writer(csvfile)
+            writer.writerows(output_rows)
+            logging.warning("Successfully updated data from VN on %s" % yesterday)
+
+
+def collect_data():
+    get_data_jhu()
+    get_data_ecdc()
+    get_data_vn()
+
+
+# everyday do at 15 : 00 PM
+schedule.every().day.at("15:00").do(collect_data)
+while True:
+    schedule.run_pending()
+    time.sleep(60)  # wait one minute
