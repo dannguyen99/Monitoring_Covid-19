805caaa81be321f997aa41cde18e0ef277f105de
huannguyen2008 <huannguyen2299@gmail.com>
2020-07-05 15:51:00 +0700

    fix things

--- web/urls.py
+++ web/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("vietnam", views.vietNamView, name="vietnam"),
+    path("vietnam", views.vietnam_view, name="vietnam"),
     path("europe", views.euView, name="europe"),
     path("us", views.us_view, name="us"),
     path("test", views.test, name="test")
